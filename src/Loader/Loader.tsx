9c3beaf4bbf8d150a2a13a552929312c0fc75a33
austine20 <81771396+austine20@users.noreply.github.com>
2023-04-21 14:55:36 +0100

    Add files via upload

---
+++ src/Loader/Loader.tsx
@@ -0,0 +1,11 @@
+import "../Loader/loader.css";
+
+const Loader = () => {
+  return (
+    <main className="wrapper-loader">
+      <div className="loader" />
+    </main>
+  );
+};
+
+export default Loader;
